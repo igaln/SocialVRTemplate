f065315781e9dc44883ee2d364403f34a02ab099
igal nassima <inassima@gmail.com>
2019-11-17 19:47:23 -0500

    ik addition

---
+++ Assets/AvatarCollisionDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+public class AvatarCollisionDetector : MonoBehaviour
+{
+
+    public NetworkedPlayer myPlayerScript;
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // myPlayerScript.updateLocalPlayerType(1);
+        //destory the collider
+        myPlayerScript.CmdspawnFireWorks();
+    }
+
+  
+}
